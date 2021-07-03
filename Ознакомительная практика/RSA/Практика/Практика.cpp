14395ca860b43f6ad742199e59325dba1e950dd0
Guseniya <86894905+Guseniya@users.noreply.github.com>
2021-07-04 00:23:22 +0300

    Add files via upload

---
+++ Ознакомительная практика/RSA/Практика/Практика.cpp
@@ -0,0 +1,53 @@
+﻿// Практика.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "math.h"
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int p, q, n, f, e, d, k;
+    {
+        cout << "Введите два числа\n";
+        cout << "(через пробелы) и нажмите Enter.\n";
+        cin >> p >> q;
+        n = p * q;
+        f = (p - 1) * (q - 1);
+        cout << n << endl;
+        cout << f << endl;
+    }
+    {
+        for ((e > 1)& (e% e == 0)& (e < f)& (f% e == 1))
+            cout << e << endl;
+        {
+            cout << "{" << e << ";" << n << "}" << "Открытый ключ " << endl;
+        }
+    }
+    {
+        d = ((k * f) + 1) / e;
+        cout << d << endl;
+        cout << "{" << d << ";" << n << "}" << "Закрытый ключ " << endl;
+        {
+            cout << "Шифрование" << endl;
+        }
+    }
+    int M, C;
+    {
+        cout << "Введите число " << endl;
+        cin >> M;
+        C = pow(M, e) % n;
+        cout << C << endl;
+    }
+}
+    
+
+
+    
+
+
+
+
+
+    
---
+++ Ознакомительная практика/Практика/Диффи Хелман/Диффи Хелман.cpp
@@ -0,0 +1,32 @@
+﻿// Диффи Хелман.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include "math.h"
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	int p, g, a, b, A, B, k;
+	{
+		cout << "Введите три числа\n";
+		cout << "(через пробелы) и нажмите Enter.\n";
+		cin >> g >> a >> p >> b;
+		A = pow(g, a) % p;
+		cout << A << endl;
+		B = pow(g, b) % p;
+		cout << B << endl;
+	}
+	{
+		if (pow(B, a) == pow(A, b))
+			k;
+		else
+			"Алгоритм не верный\n";
+	}
+}
+
+
+
+
+
